e05e8727f7fb8a99971efd591ba65b132d2ba09f
sofiiir <sofiiirodas@gmail.com>
2026-05-07 13:55:34 -0700

    Complete week 5 assignment 3

--- hw5/query_timer.sh
+++ hw5/query_timer.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
+
+## PART 1
+# set arguments
+label="$1"
+num_reps="$2"
+query="$3"
+db_file="$4"
+csv_file="$5"
+
 initialtime=$(date +%s)
 
-# i=0
-# while [ $i -lt 10 ]; do
-#     'SELECT COUNT(*) FROM Bird_nests'
-#     i=$((i+1))
-# done
+echo $initialtime
 
-# currenttime=$(date +%s)
+i=0
+while [ $i -lt $num_reps ]; do
+    duckdb $db_file "$query;" 
+    i=$((i+1))
+done
 
-# time_diff=$((echo$currenttime - echo$initialtime))
+currenttime=$(date +%s)
 
-# avg_time=$(echo$time_diff/echo$i | bc)
+time_diff=$(($currenttime - $initialtime))
 
-# echo "${avg_time}"
+avg_time=$(echo "scale=7; $time_diff/$num_reps" | bc)
+
+echo "$label" "," "$avg_time" >> $csv_file 
+
+
+## PART 2
+# In order to get timings that differed even slightly I ran the queries 
+# 10,000 times. The outer_join was the fastest. The except clause was the
+# slowest. The subquery was in between the two other times. 
