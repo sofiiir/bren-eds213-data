3cccd8ab03891e5ba174892f7164d38166405f11
sofiiir <sofiiirodas@gmail.com>
2026-06-02 10:52:20 -0700

    Walkability hw

--- hw8/walkability.sql
+++ hw8/walkability.sql
@@ -23,10 +23,34 @@ CREATE VIEW Walkability_ca AS
   .tables
 
 -- join the walkability and fip tables
-CREATE VIEW Walkind_mystate AS
+CREATE VIEW Walkind_ca AS
     SELECT * FROM Fips 
         JOIN Walkability_ca
         USING (COUNTYFP);
 
   -- view tables 
   .tables
+
+  -- compute the average walkability index per census tract
+  SELECT
+  TRACTCE,
+  COUNT(*) AS Block_count,
+  AVG(NatWalkInd) AS Walk_ind_avg
+FROM Walkind_ca
+GROUP BY TRACTCE
+ORDER BY Walk_ind_avg DESC;
+
+-- use the geospatial data to calculate the walkability index
+SELECT
+  TRACTCE,
+  COUNT(*) AS Block_count,
+  AVG(ST_Area(geom)) AS Avg_area,
+  AVG(NatWalkInd) AS Walk_ind_avg
+FROM Walkind_ca
+GROUP BY TRACTCE
+ORDER BY Walk_ind_avg DESC;
+
+-- detetermine walkability for the area of interest: Eagle Rock, Los Angeles
+SELECT * FROM Walkind_ca
+WHERE ST_Within(st_point(118.120, 34.082), geom);
+
